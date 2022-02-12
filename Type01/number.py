bb0d2cf4877891e86c0c3e4722a522d668678a71
tdlsaruar <Saruarahmed321@gmail.com>
2022-02-12 23:44:40 +0600

    syntax,variable,data,num,casting

---
+++ Type01/number.py
@@ -0,0 +1,6 @@
+x=3 #int
+y=3.0#float
+z=3j#complex
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ Type01/variables.py
@@ -0,0 +1,19 @@
+var=5
+var1='tdl saruar'
+var2=str(5)
+var3=var1+var2
+print(var,var1,var3)
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
